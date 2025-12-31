f351d0673870d601072fd94b74ed64072fd4155a
Tom Robinson <25112739+magicaljellybeans@users.noreply.github.com>
2025-12-31 12:35:14 +0000

    DFI-1397 Add scalafmt and remove example controller

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,13 +7,13 @@ object AppDependencies {
   private val hmrcMongoVersion = "2.11.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % hmrcMongoVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test
   )
 
   val it = Seq.empty
